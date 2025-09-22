bc4d508b7377397c616f64a1e323f274e1afc195
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-09-22 14:03:54 +0100

    Convert single quote to double quote

--- tests/smoke_test/test_smoke_synapse_connectivity.py
+++ tests/smoke_test/test_smoke_synapse_connectivity.py
@@ -16,7 +16,7 @@ class TestSmokeSynapseConnectivity(NotebookRunTestCase):
         notebook_parameters = dict()
 
         notebook_run_result = self.run_notebook(notebook_name, notebook_parameters)
-        exit_value = notebook_run_result["result"]["exitValue"]
+        exit_value = notebook_run_result["result"]["exitValue"].replace("'", "\"")
         if exit_value:
             failing_tests = json.loads(exit_value)
             assert not failing_tests, f"The following tests failed in the test_smoke_py_connectivity notebook : {json.dumps(failing_tests, indent=4)}"
