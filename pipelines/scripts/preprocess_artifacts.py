dc48b8adfcaeb2d176127d449cdfb86f53c3ec4c
Harrison <55688012+HarrisonBoyleThomas@users.noreply.github.com>
2025-11-10 16:09:23 +0000

    Feat/theodw 2467 open lineage config (#2199)
    * Adding sparkConfiguration: OpenLineageSparkConfig
    
    * Update workspace name to be parameterised
    
    * Update lineage config override
    
    * Update lineage config override
    
    * Update lineage config override
    
    * Remove spark config value
    
    * Perform preprocessing of json artifacts before deployment
    
    * Remove redundant override param
    
    * Add missing ABC inheritance. Use cls instead of self for classmethod. Fix incorrect type hint
    
    * Update variable name to be clearer
    
    * Improve command that sets the PYTHONPATH
    
    * Add configMergeRules

---
+++ pipelines/scripts/preprocess_artifacts.py
@@ -0,0 +1,62 @@
+import json
+from typing import Dict, Any, List, Type
+from abc import ABC, abstractmethod
+from dotenv import load_dotenv
+import os
+
+
+"""
+Script to perform preprocessing on specific workspace artifacts.
+
+# Usage
+`python3 pipelines/scripts/preprocess_artifacts.py`
+
+"""
+load_dotenv(verbose=True, override=True)
+
+
+class ArtifactPreprocessor(ABC):
+    """
+    Abstract class to suppor the preprocessing of workspace artifact json
+    """
+    @classmethod
+    def _read_artifact(cls, artifact_path: str) -> Dict[str, Any]:
+        with open(artifact_path, "r") as f:
+            return json.load(f)
+
+    @classmethod
+    def _write_artifact(cls, artifact_path: str, artifact_json: Dict[str, Any]):
+        with open(artifact_path, "w") as f:
+            json.dump(artifact_json, f, indent="\t", ensure_ascii=False)
+
+    @classmethod
+    @abstractmethod
+    def preprocess(cls):
+       """
+       Override this function to read/modify/write a json artifact
+       """
+       pass
+
+
+class OpenLineageSparkConfigPreProcessor(ArtifactPreprocessor):
+    @classmethod
+    def preprocess(cls):
+        artifact_path = "workspace/sparkConfiguration/OpenLineageSparkConfig.json"
+        env = os.environ.get("ENV")
+        function_url = os.environ.get("OL_RECEIVER_FUNCTION_URL", "")
+        artifact_json = cls._read_artifact(artifact_path)
+        artifact_json["properties"]["configs"] |= {
+            "spark.openlineage.transport.url": function_url,
+            "spark.openlineage.namespace": f"pins-synw-odw-{env.lower()}-uks"
+        }
+        cls._write_artifact(artifact_path, artifact_json)
+
+
+PRE_PROCESSORS: List[Type[ArtifactPreprocessor]] = [
+    OpenLineageSparkConfigPreProcessor
+]
+
+
+if __name__ == "__main__":
+    for preprocessor in PRE_PROCESSORS:
+        preprocessor.preprocess()
