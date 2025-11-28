fa9eb4be69d91b00535e48a129e83354201208e3
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-28 12:31:26 +0000

    Add etl processors

---
+++ odw/core/etl/etl_process.py
@@ -0,0 +1,43 @@
+from odw.core.io.data_io_factory import DataIOFactory
+from abc import ABC, abstractmethod
+from pyspark.sql import DataFrame, SparkSession
+from typing import List, Dict, Any
+
+
+class ETLProcess(ABC):
+    def __init__(self, spark: SparkSession):
+        self.spark = spark
+
+    @classmethod
+    @abstractmethod
+    def get_name(cls) -> str:
+        """
+        Return a unique name for the ETL process, to be identified as part of the factory
+        
+        :return str: A unique name for the process
+        """
+
+    def load_data(self, data_to_read: List[Dict[str, Any]]) -> Dict[str, DataFrame]:
+        data_map = dict()
+        for metadata in data_to_read:
+            data_name = metadata.get("data_name", None)
+            storage_kind = metadata.get("storage_kind", None)
+            data_format = metadata.get("data_format", None)
+            if not data_name:
+                raise ValueError(f"ETLProcess expected a data_name parameter to be passed, but this was missing")
+            if not storage_kind:
+                raise ValueError(f"ETLProcess expected a storage_kind parameter to be passed, but this was missing")
+            if not data_format:
+                raise ValueError(f"ETLProcess expected a data_format parameter to be passed, but this was missing")
+            data_io_inst = DataIOFactory.get(storage_kind)()
+            data = data_io_inst.read(**metadata)
+            data_map[data_name] = data
+        return data_map
+
+    @abstractmethod
+    def process(self, **kwargs):
+        pass
+
+    def run(self, data_to_read: List[Dict[str, Any]]):
+        source_data_map = self.load_data(data_to_read)
+        self.process(source_data=source_data_map)
---
+++ odw/core/etl/transformation/transformation_process.py
@@ -0,0 +1,6 @@
+from odw.core.etl.etl_process import ETLProcess
+from typing import Dict, List, Any
+
+
+class TransformationProcess(ETLProcess):
+    pass
