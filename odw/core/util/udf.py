497ed1887300d6d8649001d808cf82edf0f9d56d
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-02-13 10:58:22 +0000

    Add etl framework

---
+++ odw/core/util/udf.py
@@ -0,0 +1,13 @@
+import pyspark.sql.functions as F
+
+
+@F.udf
+def absolute(col: int):
+    """
+    Return the absolute value of an integer. e.g: absolute(-42) -> 42 , absolute(42) -> 42
+    """
+    if not isinstance(col, int):
+        return None
+    if col < 0:
+        return 0 - col
+    return col
