7810a1558814f9e2d2b7ed108cadb7405e78bda6
Frederic Jonquieres <47979498+Fred83200@users.noreply.github.com>
2026-04-10 15:25:23 +0100

    Adding all tests for appeal_attribute_matrix (#2447)
    * Adding all tests for appeal_attribute_matrix
    
    * Remove unused import (ruff)
    
    * Ruff format on the files
    
    * Adding Skeleton files
    
    * ruff format
    
    * Adding get_name method
    
    * Adding xfail and thin skeleton
    
    * Ruff format

---
+++ odw/core/etl/transformation/harmonised/appeal_attribute_matrix_harmonisation_process.py
@@ -0,0 +1,19 @@
+from odw.core.etl.transformation.harmonised.harmonsation_process import HarmonisationProcess
+from typing import Any
+
+
+class AppealAttributeMatrixHarmonisationProcess(HarmonisationProcess):
+    OUTPUT_TABLE = "ref_appeal_attribute_matrix"
+
+    def __init__(self, spark):
+        super().__init__(spark)
+        self.spark = spark
+
+    def get_name(self) -> str:
+        return "appeal_attribute_matrix_harmonisation_process"
+
+    def load_data(self) -> dict[str, Any]:
+        raise NotImplementedError("AppealAttributeMatrixHarmonisationProcess.load_data() has not been implemented yet.")
+
+    def process(self, source_data: dict[str, Any]):
+        raise NotImplementedError("AppealAttributeMatrixHarmonisationProcess.process() has not been implemented yet.")
