6a91d7cd589f13eaae2085c783663190f38f4b55
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-04-13 16:19:25 +0100

    Improve styling

--- odw/test/unit_test/etl/curated/test_appeal_representation_curation_process.py
+++ odw/test/unit_test/etl/curated/test_appeal_representation_curation_process.py
@@ -1,5 +1,4 @@
 from odw.core.etl.transformation.curated.appeal_representation_curation_process import AppealRepresentationCurationProcess
-from odw.core.etl.etl_result import ETLResult
 from odw.core.util.util import Util
 from odw.test.util.test_case import SparkTestCase
 from odw.test.util.session_util import PytestSparkSessionUtil
