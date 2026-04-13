5911faff78cc47d5f8bc36aea783dc7c3d638333
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-04-13 12:24:29 +0100

    Add skeleton code for appeal representation. Add unit test for appeal representation

---
+++ odw/core/etl/transformation/curated/appeal_representation_curation_process.py
@@ -0,0 +1,13 @@
+from odw.core.etl.transformation.curated.curation_process import CurationProcess
+from typing import Any
+
+
+class AppealRepresentationCurationProcess(CurationProcess):
+    def get_name(self) -> str:
+        return "Appeal Representation Curation"
+
+    def load_data(self) -> dict[str, Any]:
+        raise NotImplementedError("AppealRepresentationCurationProcess.load_data() has not been implemented yet.")
+
+    def process(self, source_data: dict[str, Any]):
+        raise NotImplementedError("AppealRepresentationCurationProcess.process() has not been implemented yet.")
