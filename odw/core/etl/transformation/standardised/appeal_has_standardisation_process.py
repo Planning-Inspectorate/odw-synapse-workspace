f795bfe3cf1ae4e6794d6061b7003d174232453a
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-05-11 17:02:23 +0100

    Update appeal has standardised process integration test

--- odw/core/etl/transformation/standardised/appeal_has_standardisation_process.py
+++ odw/core/etl/transformation/standardised/appeal_has_standardisation_process.py
@@ -4,6 +4,25 @@ from odw.core.etl.transformation.standardised.standardisation_process import Sta
 
 class AppealHasStandardisationProcess(StandardisationProcess):
     OUTPUT_TABLE = "horizon_appeal_has"
+    STANDARDISED_DB = "odw_standardised_db"
+    STANDARDISED_CASES_SPECIALISMS = "cases_specialisms"
+    STANDARDISED_HORIZON_CASES_S78 = "horizoncases_has"
+    STANDARDISED_VW_CASE_DATES = "vw_case_dates"
+    STANDARDISED_CASE_DOCUMENT_DATES_DATES = "casedocumentdatesdates"
+    STANDARDISED_CASE_SITE_STRINGS = "casesitestrings"
+    STANDARDISED_ADDITIONAL_FIELDS = "vw_additionalfields"
+    STANDARDISED_HORIZON_ADVERT_ATTRIBUTES = "horizon_advert_attributes"
+    STANDARDISED_HORIZON_SPECIALIST_CASE_DATES = "horizon_specialist_case_dates"
+    STANDARDISED_PLANNING_APP_STRINGS = "PlanningAppStrings"
+    STANDARDISED_PLANNING_APP_DATES = "PlanningAppDates"
+    STANDARDISED_LEAD_CASE = "BIS_LeadCase"
+    STANDARDISED_CASE_STRINGS = "CaseStrings"
+    STANDARDISED_HORIZON_CASE_INFO = "horizon_case_info"
+    STANDARDISED_HORIZON_CASE_DATES = "horizon_case_dates"
+    STANDARDISED_HORIZON_APPEALS_ADDITIONAL_DATA = "horizon_appeals_additional_data"
+    STANDARDISED_ADD_ADDITIONAL_DATA = "vw_addadditionaldata"
+    STANDARDISED_TYPE_OF_PROCEDURE = "typeofprocedure"
+    STANDARDISED_TYPE_OF_LEVEL = "TypeOfLevel"
 
     def __init__(self, spark):
         super().__init__(spark)
