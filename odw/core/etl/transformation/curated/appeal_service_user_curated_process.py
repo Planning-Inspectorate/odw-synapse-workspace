b3860fc2d07b23fed0f04e432602a9c207303478
frederic jonquieres <fjonquieres83@gmail.com>
2026-05-11 15:18:47 +0100

    Adding unit/int tests service_user CUR

---
+++ odw/core/etl/transformation/curated/appeal_service_user_curated_process.py
@@ -0,0 +1,20 @@
+from typing import Any
+from odw.core.etl.transformation.curated.curation_process import CurationProcess
+
+
+class AppealServiceUserCuratedProcess(CurationProcess):
+    OUTPUT_TABLE = "appeal_service_user"
+    SOURCE_TABLE = "service_user"
+
+    def __init__(self, spark):
+        super().__init__(spark)
+        self.spark = spark
+
+    def get_name(self) -> str:
+        return "appeal_service_user_curated_process"
+
+    def load_data(self) -> dict[str, Any]:
+        raise NotImplementedError("AppealServiceUserCuratedProcess.load_data() has not been implemented yet.")
+
+    def process(self, source_data: dict[str, Any]):
+        raise NotImplementedError("AppealServiceUserCuratedProcess.process() has not been implemented yet.")
