ea3d97855926d5ea44a97f27f1dabc6e4bea0b8b
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-06-16 15:12:20 +0100

    Add missing etl process to the factory

--- odw/core/etl/etl_process_factory.py
+++ odw/core/etl/etl_process_factory.py
@@ -29,6 +29,8 @@ from odw.core.etl.transformation.harmonised.horizon_pins_inspector_harmonisation
 from odw.core.etl.transformation.curated.pins_inspector_curated_process import PinsInspectorCuratedProcess
 from odw.core.etl.transformation.harmonised.nsip_project_harmonisation_process import NsipProjectHarmonisationProcess
 from odw.core.etl.transformation.harmonised.nsip_invoice_harmonisation_process import NsipInvoiceHarmonisationProcess
+from odw.core.etl.transformation.curated.nsip_invoice_curated_process import NsipInvoiceCuratedProcess
+from odw.core.etl.transformation.curated.nsip_project_curated_process import NsipProjectCuratedProcess
 from typing import Dict, List, Set, Type
 import json
 
@@ -64,7 +66,9 @@ class ETLProcessFactory:
         HorizonPinsInspectorHarmonisationProcess,
         PinsInspectorCuratedProcess,
         NsipProjectHarmonisationProcess,
-        NsipInvoiceHarmonisationProcess
+        NsipInvoiceHarmonisationProcess,
+        NsipInvoiceCuratedProcess,
+        NsipProjectCuratedProcess
     }
 
     @classmethod
