0b17fe219635a72497d5b70211e26269977b6170
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-07-20 13:18:55 +0100

    Remove commented out entities

--- odw/core/etl/historical_anonymisation/historical_anonymisation_process.py
+++ odw/core/etl/historical_anonymisation/historical_anonymisation_process.py
@@ -19,62 +19,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
     SOURCE_CONTAINER = "odw-standardised"
     SINK_CONTAINER = "odw-anonymised"
     _ENTITY_CONFIG = {
-        # "nsip-document": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/nsip-document",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_nsip_document",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["documentId"]
-        # },  # Service bus
-        # "nsip-exam-timetable": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/nsip-exam-timetable",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_nsip_exam_timetable",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["caseReference"]
-        # },  # Service bus
-        # "nsip-representation": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/nsip-representation",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_nsip_representation",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["representationId"]
-        # },  # Service bus
-        # "appeal-event": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/appeal-event",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_appeal_event",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["eventId"]
-        # },  # Service bus
-        # "application-update": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/application-update",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_application_update",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["id"]
-        # },  # Service bus
-        # "nsip-project-update": { # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/nsip-project-update",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_nsip_project_update",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["id"]
-        # },  # Service bus
-        # "service-user": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/service-user",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_service_user",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["id"]
-        # },  # Service bus
-        # "nsip-project": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/nsip-project",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_nsip_project",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["caseId"]
-        # },  # Service bus
         "appeal-has": {
             "raw_blob_path": "ServiceBus/appeal-has",
             "raw_blob_format": "json",
@@ -83,13 +27,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "primary_keys": ["caseReference"],
             "cols_to_revert_to_raw": ["siteAddressTown", "newConditionDetails"],
         },  # Service bus
-        # "appeal-s78": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/appeal-s78",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_appeal_s78",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["caseReference"]
-        # },  # Service bus
         "nsip-subscription": {
             "raw_blob_path": "ServiceBus/nsip-subscription",
             "raw_blob_format": "json",
@@ -98,49 +35,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "primary_keys": ["subscriptionId", "message_enqueued_time_utc"],
             "cols_to_revert_to_raw": ["emailAddress"],
         },  # Service bus
-        # "appeal-event-estimate": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/appeal-event-estimate",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_appeal_event_estimate",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["id"]
-        # },  # Service bus
-        # "s51-advice": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/s51-advice",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_s51_advice",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["adviceId"]
-        # },  # Service bus
-        # "appeal-document": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/appeal-document",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_appeal_document",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["documentId"]
-        # },  # Service bus
-        # "folder": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/folder",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_folder",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["id"]
-        # },  # Service bus
-        # "appeal-representation": {  # OUT OF SCOPE
-        #     "raw_blob_path": "ServiceBus/appeal-representation",
-        #     "raw_blob_format": "json",
-        #     "standardised_blob_path": "sb_appeal_representation",
-        #     "category": "ServiceBus",
-        #     "primary_keys": ["representationId"]
-        # },  # Service bus
-        # "AIEDocumentData": {  # OUT OF SCOPE
-        #     "raw_blob_path": "AIEDocumentData",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "AIEDocumentData/aie_document_data",
-        #     "category": "AIEDocumentData",
-        #     "primary_keys": ["documentId", "filename", "documentURI", "dateCreated"],
-        #     "cols_to_revert_to_raw": ["path"],
-        # },  # From py_raw_to_std
         "entraid": {
             "raw_blob_path": "entraid",
             "raw_blob_format": "json",
@@ -149,18 +43,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "primary_keys": ["id"],
             "cols_to_revert_to_raw": ["userPrincipalName"],
         },  # From py_raw_to_std
-        # "SpecialistCaseDates": { # OUT OF SCOPE
-        #    "raw_blob_path": "Horizon",
-        #    "raw_blob_format": "",
-        #    "standardised_blob_path": "Horizon/horizon_specialist_case_dates",
-        #    "category": "Horizon",
-        # },  # Horizon
-        # "CaseInfo": { # OUT OF SCOPE
-        #    "raw_blob_path": "Horizon",
-        #    "raw_blob_format": "",
-        #    "standardised_blob_path": "Horizon/horizon_case_info",
-        #    "category": "Horizon",
-        # },  # Horizon
         "InspectorCases": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -180,45 +62,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
                 "Email",
             ],
         },  # Horizon
-        # "PlanningAppDates": { # OUT OF SCOPE
-        #    "raw_blob_path": "Horizon",
-        #    "raw_blob_format": "",
-        #    "standardised_blob_path": "Horizon/PlanningAppDates",  # This one breaks from the convention for some reason
-        #    "category": "Horizon",
-        # },  # Horizon
-        # "Horizon_AppealGrounds": { # OUT OF SCOPE
-        #    "raw_blob_path": "Horizon",
-        #    "raw_blob_format": "",
-        #    "standardised_blob_path": "Horizon/horizon_appeal_grounds",
-        #    "category": "Horizon",
-        # },  # Horizon
-        # "S62AViewCaseOfficers": { # OUT OF SCOPE
-        #    "raw_blob_path": "Horizon",
-        #    "raw_blob_format": "",
-        #    "standardised_blob_path": "Horizon/horizon_s62a_view_case_officers",
-        #    "category": "Horizon",
-        # },  # Horizon
-        # "CaseDates": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_case_dates",
-        #     "category": "Horizon",
-        #     "primary_keys": [],
-        #     "horizon_file_name": "CaseDates.csv",
-        #     "cols_to_revert_to_raw": ["ValidityStatusDate"],  # todo check
-        # },  # Horizon
-        # "Horizon_NoticeDates": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_notice_dates",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "horizon_ApplicationMadeUnderSection": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/application_made_under_section",
-        #     "category": "Horizon",
-        # },  # Horizon
         "DaRT_Inspectors": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -234,84 +77,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             ],
             "horizon_file_name": "DaRT_Inspectors.csv",
         },  # Horizon
-        # "vw_case_dates": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/vw_case_dates",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "CaseStrings": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/CaseStrings",  # This one breaks from the convention for some reason
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "AppealsAdditionalData": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/appeals_additional_data",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "HorizonAppealsDocumentMetadata": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/appeals_document_metadata",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "cases_specialisms": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/cases_specialisms",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "NSIPAdvice": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_nsip_advice",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "S62AViewCases": {  # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_s62a_view_cases",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "S62AViewCaseExtendedData": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_s62a_view_case_extended_data",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "HorizonAppealsEvent": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/horizon_appeals_event",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "CaseDocumentDatesDates": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "",
-        #     "standardised_blob_path": "Horizon/CaseDocumentDatesDates",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "vw_AdditionalFields": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/vw_AdditionalFields",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "BIS_LeadCase": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/BIS_LeadCase",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "HorizonAppealsFolder": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_appeals_folder",
-        #     "category": "Horizon",
-        # },  # Horizon
         "CaseInvolvement": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -329,18 +94,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
                 "Email",
             ],
         },  # Horizon
-        # "TypeOfLevel": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/TypeOfLevel",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "HorizonAdvertAttributes": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_advert_attributes",
-        #     "category": "Horizon",
-        # },  # Horizon
         "DaRT_LPA": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -362,12 +115,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
                 "emailAddress",
             ],
         },  # Horizon
-        # "HorizonFolder": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_folder",
-        #     "category": "Horizon",
-        # },  # Horizon
         "HorizonCases_s78": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -382,18 +129,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "horizon_file_name": "HorizonCases_s78.csv",
             "cols_to_revert_to_raw": ["caseOfficerName", "coEmailAddress"],
         },  # Horizon
-        # "ExaminationTimetable": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_examination_timetable",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "vw_AddAdditionalData": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/vw_AddAdditionalData",
-        #     "category": "Horizon",
-        # },  # Horizon
         "NSIPReleventRepresentation": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -408,12 +143,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "horizon_file_name": "NSIPReleventRepresentation.csv",
             "cols_to_revert_to_raw": ["FullName", "EmailAddress"],
         },  # Horizon
-        # "BIS_CaseSiteCategoryAdditionalStr": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/BIS_CaseSiteCategoryAdditionalStr",
-        #     "category": "Horizon",
-        # },  # Horizon
         "HorizonCases_Has": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -428,12 +157,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "horizon_file_name": "HorizonCases_Has.csv",
             "cols_to_revert_to_raw": ["caseOfficerName", "coEmailAddress"],
         },  # Horizon
-        # "TypeOfProcedure": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/TypeOfProcedure",
-        #     "category": "Horizon",
-        # },  # Horizon
         "DocumentMetaData": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -448,12 +171,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "category": "Horizon",
             "cols_to_revert_to_raw": ["representative"],
         },  # Horizon
-        # "Horizon_TypeOfReasonForCase": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/Horizon_TypeOfReasonForCase",
-        #     "category": "Horizon",
-        # },  # Horizon
         "CaseSiteStrings": {
             "raw_blob_path": "Horizon",
             "raw_blob_format": "csv",
@@ -463,30 +180,6 @@ class HistoricalAnonymisationProcess(ETLProcess):
             "category": "Horizon",
             "cols_to_revert_to_raw": ["LandUse", "Town"],
         },  # Horizon
-        # "S62AViewCaseBasicData": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_s62a_view_case_basic_data",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "PlanningAppStrings": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/PlanningAppStrings",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "S62AViewCaseDates": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_s62a_view_case_dates",
-        #     "category": "Horizon",
-        # },  # Horizon
-        # "NSIPData": { # OUT OF SCOPE
-        #     "raw_blob_path": "Horizon",
-        #     "raw_blob_format": "csv",
-        #     "standardised_blob_path": "Horizon/horizon_nsip_data",
-        #     "category": "Horizon",
-        # },  # Horizon
     }
 
     @classmethod
